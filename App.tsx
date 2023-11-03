5f6b25d6bb92b379e4146ecd346f5f379e196495
marciovin <marcioviniciusfilho760@gmail.com>
2023-11-03 11:55:16 -0300

    crinado header

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
-import { Groups } from "./src/screens/groups/index"
+import theme from "./src/theme";
+import { Groups } from "./src/screens/groups/index";
+import { StatusBar } from "react-native";
+
+import { Loading } from "./src/components/Loading";
+
+import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-   <Groups/>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor='transparent'
+      translucent
+       />
+
+     { fontsLoaded ? <Groups/> : <Loading/>}
+    </ThemeProvider>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import { Container, LoadIncator  } from "./styles";
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadIncator color="red" />
+    </Container>
+  );
+}
