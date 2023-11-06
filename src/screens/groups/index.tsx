96238bf4513260cd1ea8bc94936276a041c6e285
marciovin <marcioviniciusfilho760@gmail.com>
2023-11-06 19:07:31 -0300

    highlight

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from "../../components/Header";
+import { HighLight } from "../../components/heighligth";
+
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
       <Header/>
+      <HighLight 
+      title="Turmas"
+      subtitle="Jogue com sua turma"/>
     </Container>
   );
 }
