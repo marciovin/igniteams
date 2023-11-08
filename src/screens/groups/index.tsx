12983b47939009da54061f40f84415c3312491e0
marciovin <marcioviniciusfilho760@gmail.com>
2023-11-08 20:28:47 -0300

    estilizandolista

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "../../components/Header";
 import { HighLight } from "../../components/heighligth";
+import { GroupCard } from "../../components/GroupCard";
+import { ListEmpty } from "../../components/ListEmpty";
 
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups ] = useState([]); 
+
   return (
     <Container>
       <Header/>
+
       <HighLight 
       title="Turmas"
       subtitle="Jogue com sua turma"/>
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem = {({ item }) => (
+         <GroupCard 
+           title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+        <ListEmpty
+         message='acho que está sem turma 🤔'
+         />)}
+      />
     </Container>
   );
 }
